06d892aeca4a6036374ae2c9645483e0d47641d2
MKAWRE950 <73824679+MKAWRE950@users.noreply.github.com>
2020-12-05 17:08:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, box1,box2;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(200,390,400,20); 
+
+    box1 = new Box(150,100,50,50);
+    box2 = new Box(200,300,50,100);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    
+    //ellipseMode(RADIUS);
+    //ellipse(ball.position.x, ball.position.y, 20, 20);
+
+    ground.display();
+
+    box1.display();
+    box2.display();
+}
+
